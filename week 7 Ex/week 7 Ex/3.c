d834a7bb3be2ce716291178e90d4af05a6247584
ppeanut450 <ppeanut450@gmail.com>
2020-10-13 16:30:49 +0900

    Q3

--- week 7 Ex/week 7 Ex/3.c
+++ week 7 Ex/week 7 Ex/3.c
@@ -5,9 +5,9 @@ struct student {
 	double grade;
 };
 
-int equal(struct student s1, struct student s2)
+int equal(struct student *p1, struct student *p2)
 {
-	if (s1.number == s2.number)
+	if (p1->number == p2->number)
 		return 1;
 	else
 		return 0;
@@ -18,7 +18,7 @@ int main(void)
 	struct student a = { 1,"hong", 3.8 };
 	struct student b = { 2,"kim",4.0 };
 
-	if (equal(a, b) == 1)
+	if (equal(&a, &b) == 1)
 		printf("같은 학생 \n");
 	else
 		printf("다른 학생 \n");
