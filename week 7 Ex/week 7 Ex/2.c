659785cc3a4868cd7bc9a1e854735fcf67c6103b
ppeanut450 <ppeanut450@gmail.com>
2020-10-13 16:27:51 +0900

    Q3

--- week 7 Ex/week 7 Ex/2.c
+++ week 7 Ex/week 7 Ex/2.c
@@ -9,7 +9,7 @@ struct student {
 	int number;
 	char name[20];
 	double grade;
-	struct date dob;
+	struct date *dob;
 };
 
 int main(void)
@@ -17,12 +17,12 @@ int main(void)
 	struct date d = { 5, 3, 2000 };
 	struct student s = { 20193012, "박은서", 4.5 };
 	
-	s.dob = d;
+	s.dob = &d;
 
 	printf("학번: %d\n", s.number);
 	printf("이름: %s\n", s.name);
 	printf("학점: %f\n", s.grade);
-	printf("생년월일: %d년 %d월 %d일\n", s.dob.year, s.dob.month, s.dob.day);
+	printf("생년월일: %d년 %d월 %d일\n", s.dob->year, s.dob->month, s.dob->day);
 	
 	return 0;
 }
---
+++ week 7 Ex/week 7 Ex/3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+struct student {
+	int number;
+	char name[20];
+	double grade;
+};
+
+int equal(struct student s1, struct student s2)
+{
+	if (s1.number == s2.number)
+		return 1;
+	else
+		return 0;
+}
+
+int main(void)
+{
+	struct student a = { 1,"hong", 3.8 };
+	struct student b = { 2,"kim",4.0 };
+
+	if (equal(a, b) == 1)
+		printf("같은 학생 \n");
+	else
+		printf("다른 학생 \n");
+
+	return 0;
+}
