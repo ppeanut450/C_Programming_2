fb093f9b0d611ddec6583be360ddeb1c1f942cf5
ppeanut450 <ppeanut450@gmail.com>
2020-10-17 20:32:15 +0900

    Q5

---
+++ week 7 pro/2학기 7주차/2학기 7주차/5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct point 
+{
+	int x, y;
+};
+
+int equal(struct point p1, struct point p2)
+{
+	if (p1.x == p2.x && p1.y == p2.y)
+		return 1;
+	else
+		return 0;
+}
+
+int main(void)
+{
+	struct point p1 = { 1, 2 };
+	struct point p2 = { 3, 5 };
+	
+	if (equal(p1, p2) == 1)
+		printf("(%d, %d) == (%d, %d)", p1.x, p1.y, p2.x, p2.y);
+	else
+		printf("(%d, %d) != (%d, %d)", p1.x, p1.y, p2.x, p2.y);
+}
