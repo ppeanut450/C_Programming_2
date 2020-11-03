7fd6d787b6745ce3a1063ab09cb68d0146caf4e2
ppeanut450 <ppeanut450@gmail.com>
2020-11-04 02:09:46 +0900

    Q2

---
+++ week 10 Ex/2학기 10주차 실습/2학기 10주차 실습/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int* list;
+	int i, students;
+	printf("학생의 수: ");
+	scanf_s("%d", &students);
+	list = (int*)malloc(students * sizeof(int));
+	if (list == NULL)
+	{
+		printf("동적 메모리 할당 오류\n");
+		exit(1);
+	}
+	for (i = 0; i < students; i++)
+	{
+		printf("학생 #%d 성적: ", i + 1);
+		scanf_s("%d", &list[i]);
+	}
+	printf("================================\n");
+	for (i = 0; i < students; i++)
+	{
+		printf("학생 #%d 성적: %d\n", i + 1, list[i]);
+	}
+	printf("=================================\n");
+	free(list);
+	return 0;
+}
