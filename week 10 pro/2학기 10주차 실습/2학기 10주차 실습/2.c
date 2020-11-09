1a1621e84d48335a936ef2d39d47d77b02894da2
ppeanut450 <ppeanut450@gmail.com>
2020-11-09 23:04:17 +0900

    Q2 ing

--- week 10 pro/2학기 10주차 실습/2학기 10주차 실습/2.c
+++ week 10 pro/2학기 10주차 실습/2학기 10주차 실습/2.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main(void)
 {
 	int n, i;
-	char* A;
+	
 	printf("문자열의 개수: ");
 	scanf_s("%d", &n);
 
 	char** A = NULL;
+
 	if (A = malloc(n * sizeof(char*)) != NULL)
 	{
-		printf("[ ");
 		for (i = 0; i < n; i++)
-		{
 			A[i] = (char*)malloc(100);
-			printf("\"문자열 %d\"", i);
-		}
-		printf(" ]");
 	}
 
-	return 0;
-	
+	printf("[ ");
+
+	for (i = 0; i < n; i++)
+		printf("\"문자열 %d\"", i);
+	printf(" ]");
+
+	free(A);
+	for (i = 0; i < n; i++)
+	{
+		free(A[i]);
+	}
+
+	return 0;	
 }
