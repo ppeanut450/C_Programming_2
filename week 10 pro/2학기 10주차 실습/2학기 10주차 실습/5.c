ebf33e0e153576d7b3d93f13639f1d2ca80fcb0c
ppeanut450 <ppeanut450@gmail.com>
2020-11-10 00:49:00 +0900

    Q5

--- week 10 pro/2학기 10주차 실습/2학기 10주차 실습/5.c
+++ week 10 pro/2학기 10주차 실습/2학기 10주차 실습/5.c
@@ -1,51 +1,58 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//typedef struct NODE {
-//	int num;
-//	struct NODE* link;
-//} NODE;
-//
-//int main(void)
-//{
-//	int num = 0;
-//	NODE* list = NULL;
-//	NODE* prev = NULL;
-//	NODE* p = NULL;
-//	NODE* next = NULL;
-//
-//	while (1)
-//	{
-//		printf("양의 정수를 입력하세요(종료 -1): ");
-//		scanf_s("%d", &num);
-//
-//		if (num == -1)
-//			break;
-//
-//		p = (NODE*)malloc(sizeof(NODE));
-//		
-//		p->num = num;
-//		if (list == NULL)
-//			list = 0;
-//		else
-//			prev->link = p;
-//		p->link = NULL;
-//		prev = p;
-//	}
-//
-//	p = list;
-//
-//	while (p != NULL)
-//	{
-//		printf("%d ", p->num);
-//		p = p->link;
-//	}
-//	p = list;
-//	while (p != NULL)
-//	{
-//		next = p->link;
-//		free(p);
-//		p = next;
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct NODE {
+	int num;
+	struct NODE* link;
+} NODE;
+
+int main(void)
+{
+	int num = 0;
+	NODE* list = NULL;
+	NODE* prev = NULL;
+	NODE* p = NULL;
+	NODE* next = NULL;
+
+	while (1)
+	{
+		printf("양의 정수를 입력하세요(종료 -1): ");
+		scanf_s("%d", &num);
+
+		if (num == -1)
+			break;
+
+		p = (NODE*)malloc(sizeof(NODE));
+		
+		p->num = num;
+		if (list == NULL)
+			list = p;
+		else
+			prev->link = p;
+		p->link = NULL;
+		prev = p;
+	}
+
+	p = list;
+
+	while (p != NULL)
+	{
+		printf("%d ", p->num);
+
+		if (p->link != NULL)
+			printf("-> ");
+
+		p = p->link;
+	}
+
+	p = list;
+
+	while (p != NULL)
+	{
+		next = p->link;
+		free(p);
+		p = next;
+	}
+
+	return 0;
+}
